b78dd052040eac3f936d8602453c8d20ea969f36
Pierre RAFFA <pierreraffa@192.168.1.246>
2023-09-02 17:03:11 +0100

    Implement Action

--- .github/actions/k6-prometheus-action/entrypoint.sh
+++ .github/actions/k6-prometheus-action/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-testId="${ENVIRONMENT}_$(date --utc +%F_%T)"
+testId="${INPUT_ENVIRONMENT}_$(date --utc +%F_%T)"
 echo "##[set-output name=testId;]$(echo ${testId})"
-K6_PROMETHEUS_RW_SERVER_URL=$PROMETHEUS_REMOTE_WRITE_URL \
+K6_PROMETHEUS_RW_SERVER_URL=${INPUT_PROMETHEUS-REMOTE-WRITE-URL} \
 K6_PROMETHEUS_RW_TREND_STATS='p(99),p(95),p(90),max,min,avg,count,sum' \
 k6 run \
-    -e PROJECT=$PROJECT \
-    -e ENVIRONMENT=$ENVIRONMENT \
+    -e PROJECT=$INPUT_PROJECT \
+    -e ENVIRONMENT=$INPUT_ENVIRONMENT \
     -e TEST_ID=${testId} \
-    -e AUTHOR=$AUTHOR \
-    $RUN_FLAGS \
+    -e AUTHOR=$INPUT_AUTHOR \
+    ${INPUT_RUN-FLAGS} \
     --out xk6-prometheus-rw \
-    $SCRIPT_FILEPATH \
-    $ENV_VARS_FLAG
+    ${INPUT_SCRIPT-FILEPATH} \
+    ${INPUT_ENV-VARS-FLAG}
