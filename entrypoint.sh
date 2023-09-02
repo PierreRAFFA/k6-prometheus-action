ab3b89b1a231db6a8b8e7dbb4fa7c75bc5720527
Pierre RAFFA <pierreraffa@192.168.1.246>
2023-09-02 16:42:54 +0100

    Implement Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ testId="${ENVIRONMENT}_$(date --utc +%F_%T)"
 echo "##[set-output name=testId;]$(echo ${testId})"
 K6_PROMETHEUS_RW_SERVER_URL=$PROMETHEUS_REMOTE_WRITE_URL \
 K6_PROMETHEUS_RW_TREND_STATS='p(99),p(95),p(90),max,min,avg,count,sum' \
-k6 run \
+/usr/local/bin/k6 run \
     -e PROJECT=$PROJECT \
     -e ENVIRONMENT=$ENVIRONMENT \
     -e TEST_ID=${testId} \
